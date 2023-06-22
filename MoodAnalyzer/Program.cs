d93b5dca2001e5def1bb877512fa086cf2b02171
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-22 09:56:09 +0530

    [Ritvik]Add: Given Mood should return that mood

--- MoodAnalyzer/Program.cs
+++ MoodAnalyzer/Program.cs
@@ -2,15 +2,15 @@
 {
     public class Program
     {
-        //public static string Happy_Mood = "I am in Happy Mood";
-        //public static string Sad_Mood = "I am in Sad Mood";
+        public static string Happy_Mood = "I am in Happy Mood";
+        public static string Sad_Mood = "I am in Sad Mood";
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Mood Analyzer Problem!!\n");
 
-            //Mood moodObj = new Mood();
-            //moodObj.AnalyzeMood(Happy_Mood);
-            //moodObj.AnalyzeMood(Sad_Mood);
+            Mood moodObj = new Mood();
+            moodObj.AnalyzeMood(Happy_Mood);
+            moodObj.AnalyzeMood(Sad_Mood);
         }
     }
 }
--- MoodAnalyzerTest/UnitTest1.cs
+++ MoodAnalyzerTest/UnitTest1.cs
@@ -5,11 +5,17 @@ namespace MoodAnalyzerTest
     [TestClass]
     public class UnitTest1
     {
+        private readonly MoodAnalyzer.Mood mood;
+
+        public UnitTest1()
+        {
+            mood = new MoodAnalyzer.Mood();
+        }
+        //TestCase1.1 --- Return Sad
         [TestMethod]
-        public void TestMethod1()
+        public void Given_Mood_Sad_Should_Return_Sad()
         {
             //Arrange
-            Mood mood = new Mood();
             string expectectResult = "Sad";
 
             //Act
@@ -18,6 +24,16 @@ namespace MoodAnalyzerTest
             //Assert
             Assert.AreEqual(expectectResult,result);
 
+        }
+
+        [TestMethod]
+        public void Given_Mood_Happy_Should_Return_Happy()
+        {
+            string expectedresult = "Happy";
+
+            string result = mood.AnalyzeMood("I am in Happy Mood");
+
+            Assert.AreEqual(expectedresult,result);
         }
     }
 }
